cc73327f3a89106a51e005682b66a3c8d802929d
gsluizot <contato.gsluizot@gmail.com>
2026-08-19 21:30:09 -0300

    Started the process of verifying null data

--- consumers/process_csv.py
+++ consumers/process_csv.py
@@ -1,10 +1,35 @@
 import pika, json
+import pandas as pd
+from minio import Minio
+from io import BytesIO
 
 def process_csv(channel, method, properties, body):
     try:
-        warning = json.loads(body)
-        key_file = warning["key"]
-        print(f"New File: {key_file}")
+        event = json.loads(body)
+        # TODO: Verify nulls and notify them later
+        # TODO: Verify if there are duplicates on employee_id and notify
+        # TODO: Verify if age is less than 14 and higher than 100
+        # TODO: Verify data types
+
+        minio_client = Minio(
+        "localhost:9000",
+        access_key = "minioadmin",
+        secret_key = "minioadmin",
+        secure = False
+        )
+
+        BUCKET = event["bucket"]
+        FILE_PATH = event["key"]
+
+        response = minio_client.get_object(BUCKET, FILE_PATH)
+        data = response.read()
+        response.close()
+        response.release_conn()
+
+        df = pd.read_csv(BytesIO(data))
+
+        print(df.isnull().sum())
+        # TODO: Returns the amount of nulls if any to the final user
 
         channel.basic_ack(delivery_tag = method.delivery_tag)
     except Exception as e:
