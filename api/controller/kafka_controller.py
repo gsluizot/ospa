9a288b233a7197abfb70848d16bf46e8d6efe196
gsluizot <contato.gsluizot@gmail.com>
2026-08-27 20:53:56 -0300

    Documenting register event function + type safety

--- api/controller/kafka_controller.py
+++ api/controller/kafka_controller.py
@@ -1,7 +1,15 @@
 from flask import current_app
 from models.enums.app_extensions import AppExtensions
 
-def register_event(event):
+def register_event(event: dict):
+    """
+    Register an event at kafka stream. Event must be a dictionary. The event will be registered exactly as the dictionary used.
+
+    Example:
+    {
+        "status": "PROCESSING_NULLS_OK"
+    }
+    """
     producer = current_app.extensions[AppExtensions.KAFKA.value]
 
     producer.send(
